b355bb1a69e2fb95259dc12e9bb4e8f5610548fe
Ajay Sreedhar <steelcode468@gmail.com>
2016-07-07 12:24:46 +0530

    [added] code docs

--- ATSerial.h
+++ ATSerial.h
@@ -21,11 +21,11 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
- 
+
 #ifndef __AT_SERIAL_H__
 #define __AT_SERIAL_H__
 
-#include<Arduino.h>
+#include <Arduino.h>
 #include <SoftwareSerial.h>
 
 #ifndef UART_DELAY
@@ -41,18 +41,69 @@
 
 class ATSerial {
   protected:
+    /**
+     * UART device.
+     */
     SoftwareSerial *_uart;
+
+    /**
+     * Checks for the substring in the response received from the device.
+     *
+     * @param s the substring to be checked
+     *
+     * @return true if the provided substring is found in the response, else false
+     */
     bool _checkResponse(String);
 
   public:
+    /**
+     * Uses default parameters.
+     */
     ATSerial(void);
+
+    /**
+     * Overrides the default parameters.
+     *
+     * @param uint16_t rx   the RX pin
+     * @param uint16_t tx   the TX pin
+     * @param long baud     BAUD rate
+     */
     ATSerial(uint16_t, uint16_t, long);
 
+    /**
+     * Sends the command to test AT startup.
+     *
+     * @return true if device responds with OK, false on error
+     */
     bool isOkay(void);
+
+    /**
+     * Sends the command to restart the device.
+     *
+     * @return true if successfully restarted, false on error
+     */
     bool restart(void);
+
+    /**
+     * Checks if any bytes are available from the device and
+     * returns the number of bytes available.
+     *
+     * @return number of bytes available
+     */
     int available(void);
+
+    /**
+     * Reads next character.
+     *
+     * @return character read, -1 if none available
+     */
     int read(void);
 
+    /**
+     * Reads the characters into a string buffer.
+     *
+     * @return the string read
+     */
     String readString(void);
 };
 
