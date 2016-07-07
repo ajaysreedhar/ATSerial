b974113d21e833eb567d750f2a305a7b61e0aa61
Ajay Sreedhar <steelcode468@gmail.com>
2016-07-07 12:24:42 +0530

    [added] code docs

--- ATSerial.cpp
+++ ATSerial.cpp
@@ -21,19 +21,16 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
- 
+
 #include "ATSerial.h"
 
-ATSerial::ATSerial(void) {
-  _uart = new SoftwareSerial(DEFAULT_RX, DEFAULT_TX);
-  _uart->begin(DEFAULT_BAUD);
-}
-
-ATSerial::ATSerial(uint16_t rx, uint16_t tx, long baud) {
-  _uart = new SoftwareSerial(rx, tx);
-  _uart->begin(baud);
-}
-
+/**
+ * Checks for the substring in the response received from the device.
+ *
+ * @param s the substring to be checked
+ *
+ * @return true if the provided substring is found in the response, else false
+ */
 bool ATSerial::_checkResponse(String s) {
   if (_uart->available()) {
     String r = _uart->readString();
@@ -53,26 +50,72 @@ bool ATSerial::_checkResponse(String s) {
   return false;
 }
 
+/**
+ * Uses default parameters.
+ */
+ATSerial::ATSerial(void) {
+  _uart = new SoftwareSerial(DEFAULT_RX, DEFAULT_TX);
+  _uart->begin(DEFAULT_BAUD);
+}
+
+/**
+ * Overrides the default parameters.
+ *
+ * @param uint16_t rx   the RX pin
+ * @param uint16_t tx   the TX pin
+ * @param long baud     BAUD rate
+ */
+ATSerial::ATSerial(uint16_t rx, uint16_t tx, long baud) {
+  _uart = new SoftwareSerial(rx, tx);
+  _uart->begin(baud);
+}
+
+/**
+ * Sends the command to test AT startup.
+ *
+ * @return true if device responds with OK, false on error
+ */
 bool ATSerial::isOkay(void) {
   _uart->write("AT\r\n");
   delay(UART_DELAY);
   return _checkResponse("AT\r\nOK\n");
 }
 
+/**
+ * Sends the command to restart the device.
+ *
+ * @return true if successfully restarted, false on error
+ */
 bool ATSerial::restart(void) {
   _uart->write("AT+RST\r\n");
   delay(1000);
   return _checkResponse("OK");
 }
 
+/**
+ * Checks if any bytes are available from the device and
+ * returns the number of bytes available.
+ *
+ * @return number of bytes available
+ */
 int ATSerial::available(void) {
   return _uart->available();
 }
 
+/**
+ * Reads next character.
+ *
+ * @return character read, -1 if none available
+ */
 int ATSerial::read(void) {
   return _uart->read();
 }
 
+/**
+ * Reads the characters into a string buffer.
+ *
+ * @return the string read
+ */
 String ATSerial::readString(void) {
   return _uart->readString();
 }
